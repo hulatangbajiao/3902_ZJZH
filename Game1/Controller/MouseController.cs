2c1b77b181f53484e651564541fc74214bb0753b
Yan Zhang <kuqki@internal.ligstd.com>
2020-09-04 16:35:16 -0400

    Mouse controller

--- Game1/Controller/MouseController.cs
+++ Game1/Controller/MouseController.cs
@@ -1,17 +1,25 @@
 ﻿using Game1.Command;
-using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
-using System.Diagnostics;
 
 namespace Game1.Controller
 {
     class MouseController : IController
     {
         private ICommand quitGameCommand;
+        private ICommand[] setSpriteCommands;
 
-        public MouseController(Game instance)
+        public MouseController(MainStage instance)
         {
             quitGameCommand = new QuitGameCommand(instance);
+            setSpriteCommands = new ICommand[(int) GlobalDefinitions.SpriteModes.Invalid];
+            setSpriteCommands[(int)GlobalDefinitions.SpriteModes.StaticFixed] = new SetSpriteCommand(instance,
+                GlobalDefinitions.SpriteModes.StaticFixed);
+            setSpriteCommands[(int)GlobalDefinitions.SpriteModes.StaticVerticalMoving] = new SetSpriteCommand(instance,
+                GlobalDefinitions.SpriteModes.StaticVerticalMoving);
+            setSpriteCommands[(int)GlobalDefinitions.SpriteModes.AnimatedFixed] = new SetSpriteCommand(instance,
+                GlobalDefinitions.SpriteModes.AnimatedFixed);
+            setSpriteCommands[(int)GlobalDefinitions.SpriteModes.AnimatedHorizontalMoving] = new SetSpriteCommand(instance,
+                GlobalDefinitions.SpriteModes.AnimatedHorizontalMoving);
         }
 
         public void Update()
@@ -19,12 +27,12 @@ namespace Game1.Controller
             MouseState state = Mouse.GetState();
 
             // Mouse event should only respond within the window area
-            if (state.X < 0 || state.Y > GlobalDefinitions.GraphicsWidth ||
-                state.Y < 0 || state.Y < GlobalDefinitions.GraphicsHeight)
+            if (state.X < 0 || state.X > GlobalDefinitions.GraphicsWidth ||
+                state.Y < 0 || state.Y > GlobalDefinitions.GraphicsHeight)
             {
                 return;
             }
-            
+
             // If the right button is clicked, exit the game
             if (state.RightButton == ButtonState.Pressed)
             {
@@ -33,7 +41,25 @@ namespace Game1.Controller
             }
 
             // If the left button is clicked, handle it according to the position
-            Debug.WriteLine($"X {state.X}, Y {state.Y}");
+            if (state.LeftButton == ButtonState.Pressed)
+            {
+                if (state.X < GlobalDefinitions.GraphicsWidth / 2 && state.Y < GlobalDefinitions.GraphicsHeight / 2)
+                {
+                    setSpriteCommands[(int)GlobalDefinitions.SpriteModes.StaticFixed].Execute();
+                }
+                else if (state.X > GlobalDefinitions.GraphicsWidth / 2 && state.Y < GlobalDefinitions.GraphicsHeight / 2)
+                {
+                    setSpriteCommands[(int)GlobalDefinitions.SpriteModes.AnimatedFixed].Execute();
+                }
+                else if (state.X < GlobalDefinitions.GraphicsWidth / 2 && state.Y > GlobalDefinitions.GraphicsHeight / 2)
+                {
+                    setSpriteCommands[(int)GlobalDefinitions.SpriteModes.StaticVerticalMoving].Execute();
+                }
+                else
+                {
+                    setSpriteCommands[(int)GlobalDefinitions.SpriteModes.AnimatedHorizontalMoving].Execute();
+                }
+            }
         }
     }
 }
