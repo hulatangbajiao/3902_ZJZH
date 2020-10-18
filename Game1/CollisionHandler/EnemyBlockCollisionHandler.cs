c684e0eb8806ecc95255e1d647bf9a4f7c088307
Yan Zhang <kuqki@internal.ligstd.com>
2020-10-18 01:20:09 -0400

    detection and collision

---
+++ Game1/CollisionHandler/EnemyBlockCollisionHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1.Collision
+{
+    class EnemyBlockCollisionHandler 
+    {
+        
+        private ICollision side;
+
+        public LinkEnemyCollisionHandler(IEnemy enemy, IBlock block, ICollison side)
+        {
+            this.enemy = enemy;
+            this.block = block;
+            this.side = side;
+        }
+
+        public void Execute()
+        {
+            if (side == left)
+            {
+                enemy.State = new EnemyMovingRight();
+            }
+
+        }
+    }
+}
---
+++ Game1/Detection/EnemyBlockDetection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1.Detection
+{
+    class EnemyBlockDetection : IDetection
+    {
+        private IEnemy enemy;
+        private IBlock block;
+        public EnemyBlockDetection()
+        {
+            this.enemy = enemy;
+            this.block = block;
+        }
+        public void update()
+        {
+            Rectangle ifCollision = new Rectangle();
+            ifCollision = Rectangle.Intersect(enemy.rectangle, block.rectangle);
+            if (!ifCollision.IsEmpty)
+            {
+                new EnemyBlockCollisionHandler(enemy, block, side);
+            }
+        }
+    }
+}
