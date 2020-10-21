b7a86a51ac26278f42c3ab58212e02684e0cd95e
Yan Zhang <kuqki@internal.ligstd.com>
2020-10-21 14:25:35 -0400

    update link projectile collision detection

---
+++ Game1/CollisionHandler/LinkProjectileCollisonHandler.cs
@@ -0,0 +1,42 @@
+﻿using Game1.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1.Collision
+{
+    class LinkProjectileCollisonHandler
+    {
+        
+        private ICollision side;
+        private ILink link;
+        private IProjectile projectile;
+
+        public LinkProjectileCollisonHandler(ILink link, IProjectile projectile, ICollision side)
+        {
+            this.link = link;
+            this.projectile = projectile;
+            this.side = side;
+        }
+
+        public void Execute()
+        {
+            String s = side.Getside();
+            if (side.Equals("Left"))
+            {
+                link.TakeDamage();
+            } else if (side.Equals("Right"))
+            {
+                link.TakeDamage();
+            } else if (side.Equals("Bottom"))
+            {
+                link.TakeDamage();
+            } else if (side.Equals("Top"))
+            {
+                link.TakeDamage();
+            } 
+        }
+    }
+}
