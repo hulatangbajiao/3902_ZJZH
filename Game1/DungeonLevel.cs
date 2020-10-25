c41020a2027ec4b638939970c72b37e268cb4648
Franklin110120 <43932842+Franklin110120@users.noreply.github.com>
2020-10-24 22:30:32 -0400

    Add files via upload

---
+++ Game1/DungeonLevel.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace Game1
+{
+    class DungeonLevel : ILevel
+    {
+        public LevelLoader LevelLoader1 { get; set; }
+        public IMap Map { get; set; }
+        public ILink Link { get; set; }
+        public List<IRoom> Rooms { get; set; }
+        public IRoom CurrentRoom { get; set; }
+        private List<Rectangle> doors;
+
+        public DungeonLevel(ILink link, int levelNum)
+        {
+            LevelLoader1 = new LevelLoader(1);
+            Map = new Map();
+            Link = link;
+            Rooms = new List<IRoom>();
+            CurrentRoom = new Room();
+
+            Rooms = LevelLoader1.LoadRooms(Link);
+
+            if (Rooms.Count > 0)
+            {
+                CurrentRoom = Rooms[0];
+            }
+
+            foreach (IRoom room in Rooms)
+            {
+                
+                room.Boundary = new List<IBlock>
+                {
+
+                };
+            }
+
+            doors = new List<Rectangle>
+            {
+                new Rectangle(), // North, 0
+                new Rectangle(), // East, 1
+                new Rectangle(), // South, 2
+                new Rectangle()  // West, 3
+            };
+        }
+
+       
+
+        private void North()
+        {
+            if (CurrentRoom.HasNorth)
+            {
+                Map.North();
+                CurrentRoom = CurrentRoom.North;
+                Link.DestinationRectangle = new Rectangle(doors[2].X, doors[2].Y - Link.DestinationRectangle.Height, Link.DestinationRectangle.Width, Link.DestinationRectangle.Height);
+            }
+        }
+
+        private void East()
+        {
+            if (CurrentRoom.HasEast)
+            {
+                Map.East();
+                CurrentRoom = CurrentRoom.East;
+                Link.DestinationRectangle = new Rectangle(doors[3].X + doors[3].Width, doors[3].Y, Link.DestinationRectangle.Width, Link.DestinationRectangle.Height);
+            }
+        }
+
+        private void South()
+        {
+            if (CurrentRoom.HasSouth)
+            {
+                Map.South();
+                CurrentRoom = CurrentRoom.South;
+                Link.DestinationRectangle = new Rectangle(doors[0].X, doors[0].Y + doors[0].Height, Link.DestinationRectangle.Width, Link.DestinationRectangle.Height);
+            }
+        }
+
+        private void West()
+        {
+            if (CurrentRoom.HasWest)
+            {
+                Map.West();
+                CurrentRoom = CurrentRoom.West;
+                Link.DestinationRectangle = new Rectangle(doors[1].X - Link.DestinationRectangle.Width, doors[1].Y, Link.DestinationRectangle.Width, Link.DestinationRectangle.Height);
+            }
+        }
+
+        public void Other()
+        {
+            // Move to other room
+        }
+
+        public void Update()
+        {
+            CurrentRoom.Update();
+
+            if (Link.DestinationRectangle.Intersects(doors[0]))
+            {
+                North();
+            }
+            else if (Link.DestinationRectangle.Intersects(doors[1]))
+            {
+                East();
+            }
+            else if (Link.DestinationRectangle.Intersects(doors[2]))
+            {
+                South();
+            }
+            else if (Link.DestinationRectangle.Intersects(doors[3]))
+            {
+                West();
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Map.Draw(spriteBatch);
+            CurrentRoom.Draw(spriteBatch);
+        }
+    }
+}
---
+++ Game1/ILevel.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Team4_LegendOfZelda
+{
+    public interface ILevel
+    {
+        LevelLoader LevelLoader1 { get; set; }
+        IMap Map { get; set; }
+        ILink Link { get; set; }
+        System.Collections.Generic.List<IRoom> Rooms { get; set; }
+        IRoom CurrentRoom { get; set; }
+
+        void North();
+        void East();
+        void South();
+        void West();
+        void Other();
+        void Update();
+        void Draw(SpriteBatch spriteBatch);
+    }
+}
